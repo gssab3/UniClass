b5191ced897693a506755114063a79251b3b359f
AntonioCetrulo <g.cetrulo4@studenti.unisa.it>
2025-01-30 01:50:01 +0100

    Modifica e revisione dei Javadoc - Orari
    Ho modificato i javadoc del package "Orari"

--- src/main/java/it/unisa/uniclass/orari/service/dao/AgendaDAO.java
+++ src/main/java/it/unisa/uniclass/orari/service/dao/AgendaDAO.java
@@ -7,12 +7,22 @@ import jakarta.persistence.*;
 
 import java.util.List;
 
+/**
+ * Classe DAO per la gestione delle entità Agenda nel database.
+ * Fornisce metodi per recuperare, aggiungere e rimuovere agende.
+ */
 @Stateless(name = "AgendaDAO")
 public class AgendaDAO implements AgendaRemote {
 
     @PersistenceContext(unitName = "DBUniClassPU")
     private EntityManager emUniClass;
 
+    /**
+     * Trova un'agenda nel database utilizzando il suo ID.
+     *
+     * @param id L'ID dell'agenda da cercare.
+     * @return L'oggetto Agenda corrispondente all'ID.
+     */
     @Override
     public Agenda trovaAgenda(long id) {
         TypedQuery<Agenda> query = emUniClass.createNamedQuery(Agenda.TROVA_AGENDA_ID, Agenda.class);
@@ -20,6 +30,12 @@ public class AgendaDAO implements AgendaRemote {
         return query.getSingleResult();
     }
 
+    /**
+     * Trova un'agenda nel database associata a uno studente.
+     *
+     * @param studente Lo studente di cui trovare l'agenda.
+     * @return L'oggetto Agenda associato allo studente.
+     */
     @Override
     public Agenda trovaAgenda(Studente studente) {
         TypedQuery<Agenda> query = emUniClass.createNamedQuery(Agenda.TROVA_AGENDA_STUDENTE, Agenda.class);
@@ -27,17 +43,32 @@ public class AgendaDAO implements AgendaRemote {
         return query.getSingleResult();
     }
 
+    /**
+     * Recupera tutte le agende presenti nel database.
+     *
+     * @return Una lista di tutte le agende.
+     */
     @Override
     public List<Agenda> trovaTutte() {
         TypedQuery<Agenda> query = emUniClass.createNamedQuery(Agenda.TROVA_TUTTE, Agenda.class);
         return query.getResultList();
     }
 
+    /**
+     * Aggiunge o aggiorna un'agenda nel database.
+     *
+     * @param agenda L'agenda da aggiungere o aggiornare.
+     */
     @Override
     public void aggiungiAgenda(Agenda agenda) {
         emUniClass.merge(agenda);
     }
 
+    /**
+     * Rimuove un'agenda dal database.
+     *
+     * @param agenda L'agenda da rimuovere.
+     */
     @Override
     public void rimuoviAgenda(Agenda agenda) {
         emUniClass.remove(agenda);
