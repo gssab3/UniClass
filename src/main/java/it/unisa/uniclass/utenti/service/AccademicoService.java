150211ce27382fa5f8e559604cb9883734e82770
AntonioCetrulo <g.cetrulo4@studenti.unisa.it>
2025-01-30 02:19:34 +0100

    Aggiunta Javadoc Services - Utenti
    Aggiunti i Javadoc nei services del package utenti

--- src/main/java/it/unisa/uniclass/utenti/service/AccademicoService.java
+++ src/main/java/it/unisa/uniclass/utenti/service/AccademicoService.java
@@ -9,22 +9,34 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import java.util.List;
 
+/**
+ * Classe di servizio per la gestione delle operazioni relative agli accademici.
+ * Fornisce metodi per recuperare, aggiungere e rimuovere accademici.
+ */
 @Stateless
 public class AccademicoService {
 
     private AccademicoRemote accademicoDao;
 
+    /**
+     * Costruttore di default che esegue il lookup JNDI del DAO.
+     */
     public AccademicoService() {
         try {
             System.out.println(" ATTENZIONE: Sto facendo la lookup JNDI del DAO! ");
             InitialContext ctx = new InitialContext();
             accademicoDao = (AccademicoRemote) ctx.lookup("java:global/UniClass/AccademicoDAO");
-        }
-        catch(NamingException e) {
+        } catch (NamingException e) {
             throw new RuntimeException("Errore durante il lookup di AccademicoDAO", e);
         }
     }
 
+    /**
+     * Costruttore che accetta un'istanza di AccademicoRemote.
+     * Se l'istanza è null, esegue il lookup JNDI del DAO.
+     *
+     * @param academicDao L'istanza di AccademicoRemote.
+     */
     public AccademicoService(AccademicoRemote academicDao) {
         if (academicDao == null) {
             try {
@@ -38,20 +50,32 @@ public class AccademicoService {
         }
     }
 
+    /**
+     * Trova un accademico nel database utilizzando la sua matricola.
+     *
+     * @param matricola La matricola dell'accademico da cercare.
+     * @return L'oggetto Accademico corrispondente alla matricola.
+     */
     public Accademico trovaAccademicoUniClass(String matricola) {
         try {
             return accademicoDao.trovaAccademicoUniClass(matricola);
-        }
-        catch(NoResultException e) {
+        } catch (NoResultException e) {
             return null;
         }
     }
 
+    /**
+     * Trova un accademico nel database utilizzando la sua email e password.
+     *
+     * @param email L'email dell'accademico da cercare.
+     * @param pass La password dell'accademico da cercare.
+     * @return L'oggetto Accademico corrispondente all'email e alla password.
+     */
     public Accademico trovaEmailPassUniclass(String email, String pass) {
-        try{
+        try {
             Accademico accademico = accademicoDao.trovaEmailUniClass(email);
-            if(accademico != null) {
-                if(accademico.getPassword() == null || accademico.getPassword().equals(pass)) {
+            if (accademico != null) {
+                if (accademico.getPassword() == null || accademico.getPassword().equals(pass)) {
                     return accademico;
                 } else {
                     return null;
@@ -59,41 +83,77 @@ public class AccademicoService {
             } else {
                 return null;
             }
-        } catch(NoResultException e) {
+        } catch (NoResultException e) {
             return null;
         }
     }
 
+    /**
+     * Recupera tutti gli accademici presenti nel database.
+     *
+     * @return Una lista di tutti gli accademici.
+     */
     public List<Accademico> trovaTuttiUniClass() {
         return accademicoDao.trovaTuttiUniClass();
     }
 
-
+    /**
+     * Trova un accademico nel database utilizzando la sua email.
+     *
+     * @param email L'email dell'accademico da cercare.
+     * @return L'oggetto Accademico corrispondente all'email.
+     */
     public Accademico trovaEmailUniClass(String email) {
         try {
             return accademicoDao.trovaEmailUniClass(email);
-        }
-        catch(NoResultException e) {
+        } catch (NoResultException e) {
             return null;
         }
     }
 
+    /**
+     * Trova gli accademici nel database che sono attivati o disattivati.
+     *
+     * @param attivato Lo stato di attivazione degli accademici da cercare.
+     * @return Una lista di oggetti Accademico corrispondenti allo stato di attivazione.
+     */
     public List<Accademico> trovaAttivati(boolean attivato) {
         return accademicoDao.trovaAttivati(attivato);
     }
 
+    /**
+     * Aggiunge o aggiorna un accademico nel database.
+     *
+     * @param accademico L'accademico da aggiungere o aggiornare.
+     */
     public void aggiungiAccademico(Accademico accademico) {
         accademicoDao.aggiungiAccademico(accademico);
     }
 
+    /**
+     * Rimuove un accademico dal database.
+     *
+     * @param accademico L'accademico da rimuovere.
+     */
     public void rimuoviAccademico(Accademico accademico) {
         accademicoDao.rimuoviAccademico(accademico);
     }
 
+    /**
+     * Recupera tutte le email degli accademici presenti nel database.
+     *
+     * @return Una lista di email degli accademici.
+     */
     public List<String> retrieveEmail() {
         return accademicoDao.retrieveEmail();
     }
 
+    /**
+     * Cambia lo stato di attivazione di un accademico nel database.
+     *
+     * @param accademico L'accademico di cui cambiare lo stato di attivazione.
+     * @param attivazione Il nuovo stato di attivazione.
+     */
     public void cambiaAttivazione(Accademico accademico, boolean attivazione) {
         accademicoDao.cambiaAttivazione(accademico, attivazione);
     }
